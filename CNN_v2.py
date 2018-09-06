7132fac8332c0a6b754739d7221f47a241c2f2fa
Philipp <phil@lies.io>
2018-09-06 16:26:16 +0900

    Data augmentation and sampling

--- CNN_v2.py
+++ CNN_v2.py
@@ -5,6 +5,7 @@ import torchvision.transforms as transforms
 from torchvision import models
 import configparser
 import sys
+import time
 
 if __name__ == '__main__':
 
@@ -23,20 +24,32 @@ if __name__ == '__main__':
     num_classes = config['hyperparameter'].getint('classes', 4)
     batch_size = config['hyperparameter'].getint('batch size', 10)
     learning_rate = config['hyperparameter'].getfloat('learning rate', 0.01)
-    image_size = config['hyperparameter'].getint('image size', 299)
+
+    num_samples = config['files'].getint('samples', 10)
+    image_size = config['files'].getint('image size', 299)
+    training_set_percentage = config['files'].getint('training set percentage', 80)/100.0
+    rotation_angle = config['files'].getint('rotation angle', 180)
 
     print("Paremeters:\nEpochs:\t\t{num_epochs}\nBatch size:\t{batch_size}\nLearning rate:\t{learning_rate}".format(num_epochs=num_epochs, batch_size=batch_size, learning_rate=learning_rate))
 
     data_transform = transforms.Compose([
-            transforms.Resize((image_size,image_size)),
+            transforms.RandomRotation(rotation_angle),
+            transforms.RandomResizedCrop(size=image_size, scale=(0.08,0.5), ratio=(0.8,1.25)),
             transforms.ToTensor(),
         ])
 
-    train_dataset = torchvision.datasets.ImageFolder(root=config['files'].get('training path', './train'),
+    dataset = torchvision.datasets.ImageFolder(root=config['files'].get('data path', './full'),
                                                     transform=data_transform)
+    for ii in range(num_samples-1):
+        dataset += torchvision.datasets.ImageFolder(root=config['files'].get('data path', './full'), transform=data_transform)
 
-    test_dataset = torchvision.datasets.ImageFolder(root=config['files'].get('test path', './test'),
-                                                    transform=data_transform)
+    training_size = int(training_set_percentage * len(dataset))
+    if training_size >= len(dataset) or training_size <= 0:
+        training_size = int(0.8 * len(dataset))
+
+    train_dataset, test_dataset = torch.utils.data.random_split(dataset, (training_size, len(dataset)-training_size))
+
+    print('Data sets prepared. {} samples of size {}x{}. Training set {} images, test set {}.'.format(len(dataset), image_size, image_size, len(train_dataset), len(test_dataset)))
 
     # Data loader
     train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
@@ -47,6 +60,7 @@ if __name__ == '__main__':
                                             batch_size=batch_size,
                                             shuffle=False)
 
+    print('Data loaded. Setting up model.')
 
     model_ft = models.inception_v3(pretrained=False, num_classes=num_classes, aux_logits=False)
     num_ftrs = model_ft.fc.in_features
@@ -57,9 +71,13 @@ if __name__ == '__main__':
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
+    print('Model set up. Ready to train.')
+
     # Train the model
     total_step = len(train_loader)
+    start_time = time.time()
     for epoch in range(num_epochs):
+        start_epoch = time.time()
         for i, (images, labels) in enumerate(train_loader):
             images = images.to(device)
             labels = labels.to(device)
@@ -73,9 +91,10 @@ if __name__ == '__main__':
             loss.backward()
             optimizer.step()
 
-            if (i + 1) % 1 == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
-                    .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+            current_time = time.time()
+            print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, time since start {}, time in epoch {}, time remaining {}'
+                .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), current_time-start_time, current_time-start_epoch, 
+                    (current_time-start_time)/(epoch*len(train_loader)+i+1)*((num_epochs-epoch)*len(train_loader)-i)))
 
         if config['output'].getboolean('save during training', False) and ((epoch+1) % config['output'].getint('save every nth epoch', 10) == 0):
             torch.save(model.state_dict(), 'model_after_epoch_{}.ckpt'.format(epoch))
@@ -98,6 +117,7 @@ if __name__ == '__main__':
                 confusion[pred, lab] += 1            
 
         print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+        print('Classes: {}'.format(dataset.class_to_idx))
         print('Confusion matrix:\n', (confusion))
 
     # Save the model checkpoint
