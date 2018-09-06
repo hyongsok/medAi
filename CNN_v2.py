dd4512caa2f97e48dea9bac7f233f4ea0fedfdf6
Philipp <phil@lies.io>
2018-09-06 12:52:04 +0900

    v2 with inception and config file

--- CNN_v2.py
+++ CNN_v2.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from torchvision import models
-
+import configparser
+import sys
 
 if __name__ == '__main__':
 
@@ -12,11 +13,17 @@ if __name__ == '__main__':
     print(device)
 
     # Hyper parameters
-    num_epochs = 10
-    num_classes = 4
-    batch_size = 10
-    learning_rate = 0.01
-    image_size = 224
+    config = configparser.ConfigParser()
+    if len(sys.argv) > 1:
+        config.read(sys.argv[1])
+    else:
+        config.read('default.cfg')
+
+    num_epochs = config['hyperparameter'].getint('epochs', 10)
+    num_classes = config['hyperparameter'].getint('classes', 4)
+    batch_size = config['hyperparameter'].getint('batch size', 10)
+    learning_rate = config['hyperparameter'].getfloat('learning rate', 0.01)
+    image_size = config['hyperparameter'].getint('image size', 299)
 
     print("Paremeters:\nEpochs:\t\t{num_epochs}\nBatch size:\t{batch_size}\nLearning rate:\t{learning_rate}".format(num_epochs=num_epochs, batch_size=batch_size, learning_rate=learning_rate))
 
@@ -25,10 +32,10 @@ if __name__ == '__main__':
             transforms.ToTensor(),
         ])
 
-    train_dataset = torchvision.datasets.ImageFolder(root='./train',
+    train_dataset = torchvision.datasets.ImageFolder(root=config['files'].get('training path', './train'),
                                                     transform=data_transform)
 
-    test_dataset = torchvision.datasets.ImageFolder(root='./test',
+    test_dataset = torchvision.datasets.ImageFolder(root=config['files'].get('test path', './test'),
                                                     transform=data_transform)
 
     # Data loader
@@ -41,7 +48,7 @@ if __name__ == '__main__':
                                             shuffle=False)
 
 
-    model_ft = models.resnet18(pretrained=True)
+    model_ft = models.inception_v3(pretrained=False, num_classes=num_classes, aux_logits=False)
     num_ftrs = model_ft.fc.in_features
     model_ft.fc = nn.Linear(num_ftrs, num_classes)
     model = model_ft.to(device)
@@ -70,12 +77,15 @@ if __name__ == '__main__':
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                     .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
 
+        if config['output'].getboolean('save during training', False) and ((epoch+1) % config['output'].getint('save every nth epoch', 10) == 0):
+            torch.save(model.state_dict(), 'model_after_epoch_{}.ckpt'.format(epoch))
+
     # Test the model
     model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
     with torch.no_grad():
         correct = 0
         total = 0
-        confusion = torch.zeros((4,4), dtype=torch.int)
+        confusion = torch.zeros((num_classes,num_classes), dtype=torch.float)
 
         for images, labels in test_loader:
             images = images.to(device)
@@ -88,7 +98,7 @@ if __name__ == '__main__':
                 confusion[pred, lab] += 1            
 
         print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
-        print('Confusion matrix:\n', confusion)
+        print('Confusion matrix:\n', (confusion))
 
     # Save the model checkpoint
     torch.save(model.state_dict(), 'model.ckpt')
