2dcba75c44c6104052bd62c7d4641a9f9b6c1a29
Philipp <phil@lies.io>
2018-09-21 19:31:42 +0900

    Added comments

--- CNN_v3.py
+++ CNN_v3.py
@@ -371,6 +371,22 @@ def load_state( model, optimizer, config ):
     return model, optimizer, start_epoch
 
 def initialize_meters( config, start_epoch, num_epochs, num_classes ):
+    """Initialize the accuracy and loss meters and confusion matrices
+    
+    Arguments:
+        config {configparder.ConfigParser} -- configuration file
+        start_epoch {int} -- previously executed epochs
+        num_epochs {int} -- number of new epochs
+        num_classes {int} -- number of classes (= output dimensions)
+    
+    Returns:
+        torch.Array -- training loss tensor
+        torch.Array -- training accuracy tensor
+        torch.Array -- test loss tensor
+        torch.Array -- test accuracy tensor
+        torch.array -- tensor of shape (start_epoch+num_epochs, num_classes, num_classes) for confusion matrix
+    """
+
     train_loss = torch.zeros(num_epochs, dtype=torch.float)
     train_accuracy = torch.zeros(num_epochs, dtype=torch.float)
     test_loss = torch.zeros(num_epochs, dtype=torch.float)
@@ -388,6 +404,23 @@ def initialize_meters( config, start_epoch, num_epochs, num_classes ):
 
 
 def reduce_to_2_classes( confusion, class_groups ):
+    """Reduces the confusion matrix from nxn to 2x2. All classes in the 2
+    class groups will be treated as one class in the new confusion matrix. For 
+    example if a 5x5 confusion matrix with classes 0..4 should be reduced to a
+    2x2 confusion matrix where classes 0,1 are the first column and classes 2,3,4
+    are the second column, the class_groups argument would be [[0,1],[2,3,4]].
+    Confusion matrix must have the true classes in columns and the predictions in rows.
+    
+    Arguments:
+        confusion {numpy.Array} -- confusion matrix with true classes in columns
+        class_groups {list} -- list with 2 lists containing the classes to be merged
+    
+    Returns:
+        numpy.Array -- 2x2 confusion matrix with true classes in columns
+    """
+    # first build the sum the columns of the 2 groups to reduce to 2 (n,) arrays
+    # then stack them up transposed as (2,n) and repeat the sum along the columns
+    # to 2 (2,) arrays. Stack them up transposed to get the correct confusion matrix
     confusion_2class = np.vstack((np.vstack((confusion[:,class_groups[0]].sum(1),
                                          confusion[:,class_groups[1]].sum(1)))[:,class_groups[0]].sum(1), 
                                 np.vstack((confusion[:,class_groups[0]].sum(1),
@@ -396,6 +429,21 @@ def reduce_to_2_classes( confusion, class_groups ):
     return confusion_2class
 
 def save_state( model, optimizer, num_epochs, train_loss, train_accuracy, test_loss, test_accuracy, test_confusion, classes, filename ):
+    """Save the current state of the model including history of training and test performance
+    
+    Arguments:
+        model {torch.nn.Module} -- the deep neural network
+        optimizer {torch.optim.Optimizer} -- the optimizer to use for the training, e.g. Adam or SGD
+        num_epochs {int} -- the number of executed epochs  
+        train_loss {torch.Array} -- tensor of training losses
+        train_accuracy {torch.Array} -- tensor of training accuracy
+        test_loss {torch.Array} -- tensor of test losses
+        test_accuracy {torch.Array} -- tensor of test accuracy
+        test_confusion {torch.Array} -- tensor of confusion matrices
+        classes {dict} -- class dictionary
+        filename {string} -- target filename
+    """
+
     torch.save({
         'epoch': num_epochs,
         'state_dict': model.state_dict(),
@@ -409,6 +457,17 @@ def save_state( model, optimizer, num_epochs, train_loss, train_accuracy, test_l
     }, filename)
 
 def save_performance( train_loss, train_accuracy, test_loss, test_accuracy, test_confusion, classes, filename ):
+        """Save the current model performance, e.g. for visualization
+    
+    Arguments:
+        train_loss {torch.Array} -- tensor of training losses
+        train_accuracy {torch.Array} -- tensor of training accuracy
+        test_loss {torch.Array} -- tensor of test losses
+        test_accuracy {torch.Array} -- tensor of test accuracy
+        test_confusion {torch.Array} -- tensor of confusion matrices
+        classes {dict} -- class dictionary
+        filename {string} -- target filename
+    """
     torch.save({
         'train_loss': train_loss,
         'train_accuracy': train_accuracy,
